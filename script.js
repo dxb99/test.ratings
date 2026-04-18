210fb5c5dd19fd74e08a6e4ab920b56787f98c5c
dxb99 <dxb99.clan@gmail.com>
2026-04-18 01:01:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -340,6 +340,12 @@ el.innerHTML = `
       startCountdown(expiry);
     }
   }
+
+// 🔥 ALWAYS RE-INJECT SESSION MAPS AFTER RENDER
+setTimeout(() => {
+  loadSessionMaps();
+}, 0);
+  
 }
 
 document.getElementById("generateButton").onclick = generateMatchups;
